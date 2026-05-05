d0d960d549509b461bcadb4b4c84b702fc16ad53
alex-under <arv0297@gmail.com>
2026-05-05 17:30:42 -0400

    fixing carrears controller and form

--- app/controllers/carreras_controller.rb
+++ app/controllers/carreras_controller.rb
@@ -1,64 +1,69 @@
 class CarrerasController < ApplicationController
-  before_action :set_carrera, only: [:show,:delete,:edit,:update]
+  before_action :set_carrera, only: [:show, :edit, :update, :destroy]
+  before_action :set_facultades, only: [:new, :edit, :create, :update]
   layout "prueba"
+
+  # GET /carreras
   def index
-    @todocarrera= Carrera.all
-    @carreras= Carrera.new
-
+    @carreras = Carrera.all.order(:nombrecarrera)
+    @carrera = Carrera.new
   end
-  
-  def new
-    @carreras= Carrera.new
-   end
 
-
-
+  # GET /carreras/1
   def show
-    @carrera= Carrera.find(params[:id])
   end
 
-  def delete
-    #@user.destroy
-    
-    @carrera.estado = false
-    @carrera.save
-      redirect_to carreras_path, success: "Carrera no existente"
+  # GET /carreras/new
+  def new
+    @carrera = Carrera.new
   end
 
-  def update
-      if @carrera.update(carrera_params)
-        redirect_to carrera_path, success: "Se Actualizaron los datos"
-      else
-        render :edit ,danger: "Modifique por valores válidos"
-      end
-  end
-
+  # GET /carreras/1/edit
   def edit
   end
-   
 
-
+  # POST /carreras
   def create
-    @carrera= Carrera.new(carrera_params)
+    @carrera = Carrera.new(carrera_params)
+
     if @carrera.save
-      redirect_to carreras_path, success: "Se creo correctamente"
+      redirect_to carreras_path, notice: 'Carrera creada exitosamente.'
     else
-      redirect_to new_carrera_path, danger: "Complete correctamente el formulario"
+      flash.now[:alert] = 'Error al crear la carrera. Verifique los datos.'
+      render :new
     end
   end
 
-
-
-
-
- private
-  def carrera_params
-  params.require(:carrera).permit(:nombrecarrera, :codigo, :facultad_id, :estado)
+  # PATCH/PUT /carreras/1
+  def update
+    if @carrera.update(carrera_params)
+      redirect_to @carrera, notice: 'Carrera actualizada exitosamente.'
+    else
+      flash.now[:alert] = 'Error al actualizar la carrera. Verifique los datos.'
+      render :edit
+    end
   end
+
+  # DELETE /carreras/1
+  def destroy
+    if @carrera.update(estado: false)
+      redirect_to carreras_path, notice: 'Carrera desactivada exitosamente.'
+    else
+      redirect_to carreras_path, alert: 'Error al desactivar la carrera.'
+    end
+  end
+
+  private
 
   def set_carrera
     @carrera = Carrera.find(params[:id])
   end
 
+  def set_facultades
+    @facultades = Facultad.all.order(:nombrefacultad)
+  end
 
+  def carrera_params
+    params.require(:carrera).permit(:nombrecarrera, :codigo, :facultad_id, :estado)
+  end
 end
